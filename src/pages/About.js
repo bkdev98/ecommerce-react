c9044c6ab1b0bcd9e9fc6fa2caae3686afe3ca2f
Quốc Khánh <khanhbq@innoteq.vn>
2019-01-18 17:00:08 +0700

    add about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import Wrapper from '../components/Wrapper';
+import logo from '../images/logo.png';
 
 const About = () => (
-  <Wrapper>About Page</Wrapper>
+  <Wrapper>
+    <img src={logo} alt='logo' />
+    <p>Chúng tôi là đội Tên lửa bảo vệ ngân hà</p>
+    <p>Một tương lai tương sáng đang chờ đợi chúng tôi!</p>
+  </Wrapper>
 );
 
 export default About;
